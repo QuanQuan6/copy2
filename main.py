f461cc0b1a8fea98d22e861d5c0d945b90a8bffe
liufengrong <1733535832@qq.com>
2022-06-04 22:23:13 +0800

    读取数据

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from data_read import read_data, FJSP_Data
-import geatpy as ga
+from data_read_copy import read_data
+from FJSP_Data import FJSP_Data
+from Gene import Gene
 
 data_path = 'Monaldo\Fjsp\Job_Data\Barnes\Text\mt10c1.txt'
 
 data = read_data(data_path)
-data.display_info()
+#data.display_info(True)
+encode = Gene(data)
+encode.show()
