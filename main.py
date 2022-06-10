0aef138215e39c8f524c45f8a0de989369991206
liufengrong <1733535832@qq.com>
2022-06-10 21:59:25 +0800

    2022-6-10-21-59

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from FJSP_Data import FJSP_Data
 from population import population
 import time
 import numpy as np
+
 data_path = 'Monaldo\Fjsp\Job_Data\Brandimarte_Data\Text\Mk01.fjs'
+data_path = 'data.txt'
 
 data = read_data(data_path)
 # data.display_info(True)
 begin_time = time.time()
-encode = population(data, 10)
+encode = population(data, 1000, 1000, 1000)
 best = encode.GA()
-encode.show_gantt_chart(encode.MS[best], encode.OS[best])
+encode.print(encode.MS[best], encode.OS[best])
+encode.show_gantt_chart(encode.MS[best], encode.OS[best], figsize=(14, 4))
 # encode.show_gantt_chart()
 print(time.time()-begin_time)
