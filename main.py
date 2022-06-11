01779ebf3446d4b58d3d9ad206b8c79aceee194a
liufengrong <1733535832@qq.com>
2022-06-11 18:12:24 +0800

    2022-6-11-18-12

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from colorama import init
 from read_data import read_data
-from FJSP_Data import FJSP_Data
 from population import population
 import time
-import numpy as np
+'''
+1:自适应初始化 未完成
+2:自适应交叉概率和变异概率
+2:新种群选择策略  
+    1)锦标赛
+3:交叉方法
+    1) 均匀交叉
+    2) 工序码
+'''
 
-data_path = 'Monaldo\Fjsp\Job_Data\Brandimarte_Data\Text\Mk01.fjs'
 data_path = 'data.txt'
+data_path = 'Monaldo\Fjsp\Job_Data\Brandimarte_Data\Text\Mk06.fjs'
 
 data = read_data(data_path)
 # data.display_info(True)
 begin_time = time.time()
-encode = population(data, 1000, 1000, 1000)
-best = encode.GA()
-encode.print(encode.MS[best], encode.OS[best])
-encode.show_gantt_chart(encode.MS[best], encode.OS[best], figsize=(14, 4))
-# encode.show_gantt_chart()
+peoples = population(data,20,20,20)
+best = peoples.GA(max_step=100,max_no_new_best=100)
 print(time.time()-begin_time)
+#encode.print(encode.best_MS, encode.best_OS)
+peoples.show_gantt_chart(peoples.best_MS, peoples.best_OS, figsize=(14, 4))
---
+++ 提交/2022-6-11/main.py
@@ -0,0 +1,20 @@
+from read_data import read_data
+from population import population
+import time
+'''
+1:自适应初始化 未完成
+2:自适应交叉概率和变异概率
+2:新种群选择策略  
+    1)锦标赛
+'''
+
+data_path = 'data.txt'
+
+data = read_data(data_path)
+# data.display_info(True)
+begin_time = time.time()
+peoples = population(data, 100, 100, 100)
+best = peoples.GA(max_step=20)
+print(time.time()-begin_time)
+peoples.print(peoples.best_MS,peoples.best_OS)
+peoples.show_gantt_chart(peoples.best_MS, peoples.best_OS, figsize=(14, 4))
