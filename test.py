67c4ea92db087e0873ca55ac5e3bf6b43a42bef1
liufengrong <1733535832@qq.com>
2022-06-12 17:52:18 +0800

    1

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from read_data import read_data
-from population import population, uniform_crossover_random
-from population import uniform_crossover
+from population import *
 import time
-import numba
 import numpy as np
+import os
+import psutil
 '''
 1:自适应初始化 未完成
 2:自适应交叉概率和变异概率   改锦标
@@ -19,16 +19,34 @@ data_path = 'Monaldo\Fjsp\Job_Data\Brandimarte_Data\Text\Mk01.fjs'
 data = read_data(data_path)
 # data.display_info(True)
 
-peoples = population(data, 60, 30, 10)
+peoples = population(data, 5)
+jobs_operations = peoples.data.jobs_operations
+jobs_num = peoples.data.jobs_num
+machines_num = peoples.data.machines_num
+max_operations = jobs_operations.max()
+jobs_order = np.arange(jobs_num)
+MS_positions = np.empty(
+    shape=(jobs_num, max_operations), dtype=int)
+jobs_operations_detail = peoples.data.jobs_operations_detail
+jobs_operations_detail = initial_jobs_operations_detail(jobs_operations_detail)
+candidate_machine = peoples.data.candidate_machine
+MS = np.empty(shape=(peoples.size, peoples.length), dtype=int)
+candidate_machine_index = peoples.data.candidate_machine_index
+global_first_index = 0
+global_rear_index = peoples.global_size
+machine_time = np.zeros(
+    shape=(1, machines_num), dtype=int).flatten()
+print(MS)
+initial_MS_global_selection(MS, global_first_index, global_rear_index, MS_positions, jobs_order,
+                            jobs_operations, jobs_operations_detail, machine_time)
+print(MS)
 begin_time = time.time()
-for i in range(2000):
-    uniform_crossover(peoples.MS, 1, 1)
-print(time.time()-begin_time)
-print(peoples.best_score)
-begin_time = time.time()
-for i in range(2000):
-    positions = np.array([range(peoples.length)]).flatten()
-    uniform_crossover_random(peoples.MS, 1, 1,positions)
-print(time.time()-begin_time)
+for i in range(100000):
+    initial_MS_position(MS_positions, jobs_operations)
+end_time = time.time()
+print("运行了{}秒".format(end_time-begin_time))
+print(MS_positions.shape[0])
+
+
 #encode.print(encode.best_MS, encode.best_OS)
 #peoples.show_gantt_chart(peoples.best_MS, peoples.best_OS, figsize=(14, 4))
